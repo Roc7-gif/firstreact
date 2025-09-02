c262f0d9b110868729e670083e432dd25dba1570
Roc7-gif <zosroc@gmail.com>
2025-09-02 16:24:02 +0100

    firstreact

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router , Routes ,Route } from "react-router-dom"
 
 import { Contact } from './pages/Contact.jsx'
 import { Events } from './pages/events.jsx'
-import { Login } from './pages/login..jsx'
+import { Login } from './pages/login.jsx'
 import { Register } from './pages/Register.jsx'
 import { AproposPage } from './pages/Apropos.jsx'
 import { Details } from './pages/Details.jsx'
